8e3d518d94334136063761822419d9c04405925b
dlanaras <70207362+dlanaras@users.noreply.github.com>
2021-06-28 07:59:45 +0200

    Removed unnecessary files

--- Scripts/tailCounter.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class tailCounter : MonoBehaviour
-{
-    public List<Vector3> listPos = new List<Vector3>();
-}
