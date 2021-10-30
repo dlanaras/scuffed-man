365b98a6f4a7e680a8408705f6ff74cb416a4659
dlanaras <dimitrislanaras04@outlook.com>
2021-10-30 20:45:37 +0200

    refactoreing code stuck on problem

--- Scripts/ConsumeThePoint.cs
+++ Scripts/ConsumeThePoint.cs
@@ -8,115 +8,114 @@ using UnityEngine.SceneManagement;
 
 public class ConsumeThePoint : MonoBehaviour
 {
-    /*  GameObject totalPointObjects = GameObject.FindWithTag("Point");
-    private void Awake() {
-        Debug.Log(totalPointObjects.name + " has " + totalPointObjects.transform.childCount + " children");
-    }    */
-    public float speed = 10f;
-    public TMP_Text score; // link to text that display
-    public GameObject player;
-    public float space = 5f;
-    private int i = 0; // score amount
-    public int pointAmount; // Amount of points to get added when consuming a point
-    [SerializeField] public GameObject pointArray;
-    private int totalPointObjects; // for now defined like this later on probably with a loop
+
+    public TMP_Text scoreTMP;
+    private int scoreAmount = 0;
+    public int spaceBetweenTails = 5;
+    public int speed = 0;
+    private int amountOfPointObjects;
+    public int pointValue;
+    public GameObject pointGameObjects;
     private int tailCounter = 0;
-    public float distance = 5f;
-    private string direction = "right";
-    private GameObject currentTail;
-    //public float spaceBetweenTails = 1.5f;
-    public List<Vector3> positionArray = new List<Vector3>();
-    // Init for different colors to be used as Text color
+
+    private Vector2 direction;
     Color tenToForty = new Color(0, 255, 0, 255);
     Color fortyToSeventy = new Color(0, 123, 123, 255);
     Color seventyToNinety = new Color(0, 0, 255, 255);
     Color ninetyNine = new Color(255, 0, 0, 255);
 
-
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Awake()
     {
+        this.amountOfPointObjects = pointGameObjects.transform.childCount;
+    }
 
-        if (other.gameObject.CompareTag("Point"))
+
+    private void Update()
+    {
+        if (Input.GetKey("up"))
         {
-            tailCounter++;
-            Destroy(other.gameObject.transform.parent.gameObject); // Destroy the Point
-            GameObject tail = new GameObject("Tail" + (Convert.ToString(tailCounter))); // create a new Tail Gameobject
-            //tail.transform.position = tail.transform.position * Vector2.right * Input.GetAxis("Horizontal") * Time.deltaTime;
+            this.direction = Vector2.up;
+            this.MakeTailsFollowHead();
+        }
+        else if (Input.GetKey("down"))
+        {
+            this.direction = Vector2.down;
+            this.MakeTailsFollowHead();
+        }
+        else if (Input.GetKey("left"))
+        {
+            this.direction = Vector2.left;
+            this.MakeTailsFollowHead();
+        }
+        else if (Input.GetKey("right"))
+        {
+            this.direction = Vector2.right;
+            this.MakeTailsFollowHead();
+        }
 
-            // Add needed tail components like collider and scriptsa
-            tail.AddComponent<BoxCollider2D>();
-            BoxCollider2D tailCollider = tail.GetComponent<BoxCollider2D>();
-            tailCollider.isTrigger = true;
-
-            //tail.AddComponent<Rigidbody2D>();
-
-
-            tail.AddComponent<SpriteRenderer>();
-            Sprite tailSprite = Resources.Load<Sprite>("Textures/ScuffedMan_128_Tail");
-            SpriteRenderer tailSpriteR = tail.GetComponent<SpriteRenderer>();
-            tailSpriteR.sprite = tailSprite;
-
-            tail.AddComponent<OnTouchedByEnemy>();
-            tail.tag = "Tail";
-
-            // HAS TAIL
-            if (tailCounter >= 1)
-            {
-                switch (direction)
-                {
-                    case "up":
-                        tail.transform.position.Set(this.transform.position[0], this.transform.position[1] - (space + tailCounter), 0);
-                        break;
-                    case "down":
-                        tail.transform.position.Set(this.transform.position[0], this.transform.position[1] + (space + tailCounter), 0);
-                        break;
-                    case "left":
-                        tail.transform.position.Set(this.transform.position[0] + (space + tailCounter), this.transform.position[1], 0);
-                        break;
-                    case "right":
-                        tail.transform.position.Set(this.transform.position[0] - (space + tailCounter), this.transform.position[1], 0);
-                        break;
-                }
-            }
-
-
-            // POINTS
-            i += pointAmount;
-            score.text = "Score: " + i.ToString();
-            totalPointObjects = pointArray.transform.childCount;
-
-            if (i > (totalPointObjects * pointAmount) * 0.10 && i < (totalPointObjects * pointAmount) * 0.40)
-            {
-                score.color = tenToForty;
-            }
-            else if (i > (totalPointObjects * pointAmount) * 0.40 && i < (totalPointObjects * pointAmount) * 0.70)
-            {
-                score.color = fortyToSeventy;
-            }
-            else if (i > (totalPointObjects * pointAmount) * 0.70 && i < (totalPointObjects * pointAmount) * 0.99)
-            {
-                score.color = seventyToNinety;
-            }
-            else if (i >= (totalPointObjects * pointAmount) * 0.99)
-            {
-                score.color = ninetyNine;
-            }
+        if (pointGameObjects.transform.childCount == 0)
+        {
+            Debug.Log("YES");
+            SceneManager.LoadScene("Won");
         }
     }
 
-    private void MakeTailsFollowHead(Vector2 direction)
+
+    private void OnTriggerEnter2D(Collider2D otherCollider2D)
     {
+
+        if (otherCollider2D.gameObject.CompareTag("Point"))
+        {
+            Destroy(otherCollider2D.gameObject.transform.parent.gameObject); // Destroy consumed point
+            this.CreateTail();
+
+            // POINTS
+            scoreAmount += pointValue;
+            PlayerPrefs.SetInt("Score", scoreAmount);
+            scoreTMP.text = "Score: " + scoreAmount.ToString();
+            this.SetScoreColorAccordingToPercentage();
+        }
+    }
+
+    private void SetScoreColorAccordingToPercentage()
+    {
+        int maxPointValue = amountOfPointObjects * pointValue;
+
+        if (scoreAmount < maxPointValue * 0.4) //40% or less amount of max points collected
+        {
+            scoreTMP.color = tenToForty;
+        }
+        else if (scoreAmount > maxPointValue * 0.4 && scoreAmount < maxPointValue * 0.7) //between 40% and 70%
+        {
+            scoreTMP.color = fortyToSeventy;
+        }
+        else if (scoreAmount > maxPointValue * 0.7 && scoreAmount < maxPointValue * 0.90) //between 70% and 90%
+        {
+            scoreTMP.color = seventyToNinety;
+        }
+        else if (scoreAmount >= maxPointValue * 0.90) //more than 90%
+        {
+            scoreTMP.color = ninetyNine;
+        }
+    }
+
+
+    private void MakeTailsFollowHead()
+    {
+        //rotation
+        //transform.rotation = Quaternion.identity;
+
         if (tailCounter > 0)
         {
             int i = tailCounter;
             Vector3 previousTailPos = new Vector3();
             foreach (GameObject tail in GameObject.FindGameObjectsWithTag("Tail"))
             {
-                if (i == tailCounter)
+                if (i == tailCounter) //if first tail
                 {
                     previousTailPos = tail.transform.position; //save tail pos
-                    tail.transform.position = this.transform.position; //tail gets moved to position of head
-                    this.transform.Translate(direction * Time.deltaTime * speed); //head moves
+                    //tail.transform.position = this.transform.position; //tail gets moved to position of head
+                    this.transform.Translate(this.direction * Time.deltaTime * speed); //head moves
 
                 }
                 else
@@ -131,38 +130,34 @@ public class ConsumeThePoint : MonoBehaviour
         }
         else
         {
-            this.transform.Translate(direction * Time.deltaTime * speed);
+            this.transform.Translate(this.direction * Time.deltaTime * speed);
         }
     }
-    private void Update()
-    {
-        //rotation
-        transform.rotation = Quaternion.identity;
-        if (Input.GetKey("up"))
-        {
-            this.MakeTailsFollowHead(Vector2.up);
-        }
-        else if (Input.GetKey("down"))
-        {
-            this.MakeTailsFollowHead(Vector2.down);
-        }
-        else if (Input.GetKey("left"))
-        {
-            this.MakeTailsFollowHead(Vector2.left);
-        }
-        else if (Input.GetKey("right"))
-        {
-            this.MakeTailsFollowHead(Vector2.right);
-        }
 
-        // No More Points
-        PlayerPrefs.SetInt("Score", i);
-        if (pointArray.transform.childCount == 0)
-        {
-            Debug.Log("YES");
-            SceneManager.LoadScene("Won");
-        }
+    private void CreateTail()
+    {
+        tailCounter++;
+
+        GameObject tail = new GameObject("Tail" + (Convert.ToString(tailCounter)));
+        tail.transform.position = this.transform.position;// * (spaceBetweenTails + tailCounter);
+        //tail.transform.position.Set(this.transform.position[0], this.transform.position[1] + spaceBetweenTails * tailCounter, 0);
+        Debug.Log(this.transform.position);
+        Debug.Log(tail.transform.position);
+
+
+        // Add needed tail components like collider and scripts
+        tail.AddComponent<BoxCollider2D>();
+        BoxCollider2D tailCollider = tail.GetComponent<BoxCollider2D>();
+        tailCollider.isTrigger = true;
+
+        tail.AddComponent<SpriteRenderer>();
+        Sprite tailSprite = Resources.Load<Sprite>("Textures/ScuffedMan_128_Tail");
+        SpriteRenderer tailSpriteR = tail.GetComponent<SpriteRenderer>();
+        tailSpriteR.sprite = tailSprite;
+
+        tail.AddComponent<OnTouchedByEnemy>();
+        tail.tag = "Tail";
+
     }
 
 }
-
