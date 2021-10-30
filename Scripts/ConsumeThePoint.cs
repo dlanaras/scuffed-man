17d953c1e66a898cb7d7ca842db0b6509b522144
dlanaras <dimitrislanaras04@outlook.com>
2021-10-30 23:12:11 +0200

    no idea how to add space between tails while movin

--- Scripts/ConsumeThePoint.cs
+++ Scripts/ConsumeThePoint.cs
@@ -11,14 +11,15 @@ public class ConsumeThePoint : MonoBehaviour
 
     public TMP_Text scoreTMP;
     private int scoreAmount = 0;
-    public int spaceBetweenTails = 5;
+    public float spaceBetweenTails = 0.1f;
     public int speed = 0;
     private int amountOfPointObjects;
     public int pointValue;
     public GameObject pointGameObjects;
     private int tailCounter = 0;
-
+    private Vector2 previousTailPos = new Vector2();
     private Vector2 direction;
+    private List<GameObject> gameObjects = new List<GameObject>();
     Color tenToForty = new Color(0, 255, 0, 255);
     Color fortyToSeventy = new Color(0, 123, 123, 255);
     Color seventyToNinety = new Color(0, 0, 255, 255);
@@ -27,6 +28,7 @@ public class ConsumeThePoint : MonoBehaviour
     private void Awake()
     {
         this.amountOfPointObjects = pointGameObjects.transform.childCount;
+        gameObjects.Add(this.gameObject);
     }
 
 
@@ -35,22 +37,22 @@ public class ConsumeThePoint : MonoBehaviour
         if (Input.GetKey("up"))
         {
             this.direction = Vector2.up;
-            this.MakeTailsFollowHead();
+            this.MakeTailsFollowHeadAndMove();
         }
         else if (Input.GetKey("down"))
         {
             this.direction = Vector2.down;
-            this.MakeTailsFollowHead();
+            this.MakeTailsFollowHeadAndMove();
         }
         else if (Input.GetKey("left"))
         {
             this.direction = Vector2.left;
-            this.MakeTailsFollowHead();
+            this.MakeTailsFollowHeadAndMove();
         }
         else if (Input.GetKey("right"))
         {
             this.direction = Vector2.right;
-            this.MakeTailsFollowHead();
+            this.MakeTailsFollowHeadAndMove();
         }
 
         if (pointGameObjects.transform.childCount == 0)
@@ -99,8 +101,7 @@ public class ConsumeThePoint : MonoBehaviour
         }
     }
 
-
-    private void MakeTailsFollowHead()
+    private void MakeTailsFollowHeadAndMove()
     {
         //rotation
         //transform.rotation = Quaternion.identity;
@@ -108,25 +109,40 @@ public class ConsumeThePoint : MonoBehaviour
         if (tailCounter > 0)
         {
             int i = tailCounter;
-            Vector3 previousTailPos = new Vector3();
-            foreach (GameObject tail in GameObject.FindGameObjectsWithTag("Tail"))
+
+            this.transform.Translate(this.direction * Time.deltaTime * speed);
+
+
+            if (i == tailCounter)
             {
-                if (i == tailCounter) //if first tail
-                {
-                    previousTailPos = tail.transform.position; //save tail pos
-                    //tail.transform.position = this.transform.position; //tail gets moved to position of head
-                    this.transform.Translate(this.direction * Time.deltaTime * speed); //head moves
-
-                }
-                else
-                {
-                    Vector3 tempPos = tail.transform.position; //save tail pos
-                    tail.transform.position = previousTailPos; //move tail to last tail location
-                    previousTailPos = tempPos; //set previouspos to actually previouspos (tempPos)
-                }
-
-                i--;
+                previousTailPos = gameObject.transform.position;
+                gameObject.transform.Translate(direction * Time.deltaTime * speed);
             }
+            else
+            {
+                Vector2 tempPos = gameObject.transform.position;
+                gameObject.transform.position = previousTailPos;
+                previousTailPos = tempPos;
+            }
+
+
+
+            /*
+                            previousTailPos = tail.transform.position; //save tail pos
+                            tail.transform.position.Set(direction[0] * Time.deltaTime * -1 * spaceBetweenTails, this.transform.position[1], 0); // = this.transform.position; //tail gets moved to position of head
+                            this.transform.Translate(this.direction * Time.deltaTime * speed); //head moves
+                            tail.transform.Translate(direction * -1 * spaceBetweenTails * Time.deltaTime);
+                            //tail.transform.Translate((direction * -1 * spaceBetweenTails) * tailCounter);
+
+
+                            Vector2 tempPos = tail.transform.position; //save tail pos
+                            tail.transform.position = previousTailPos; //move tail to last tail location
+                            previousTailPos = tempPos; //set previouspos to actually previouspos (tempPos)
+
+            */
+            i--;
+
+
         }
         else
         {
@@ -139,11 +155,9 @@ public class ConsumeThePoint : MonoBehaviour
         tailCounter++;
 
         GameObject tail = new GameObject("Tail" + (Convert.ToString(tailCounter)));
-        tail.transform.position = this.transform.position;// * (spaceBetweenTails + tailCounter);
-        //tail.transform.position.Set(this.transform.position[0], this.transform.position[1] + spaceBetweenTails * tailCounter, 0);
-        Debug.Log(this.transform.position);
-        Debug.Log(tail.transform.position);
-
+        //place new tail on opposite direction of player with distance based of spaceBetweenTails and tailCounter
+        tail.transform.position = this.transform.position;
+        tail.transform.Translate((direction * -1 * spaceBetweenTails) * tailCounter);
 
         // Add needed tail components like collider and scripts
         tail.AddComponent<BoxCollider2D>();
@@ -154,6 +168,7 @@ public class ConsumeThePoint : MonoBehaviour
         Sprite tailSprite = Resources.Load<Sprite>("Textures/ScuffedMan_128_Tail");
         SpriteRenderer tailSpriteR = tail.GetComponent<SpriteRenderer>();
         tailSpriteR.sprite = tailSprite;
+        tailSpriteR.sortingOrder = tailCounter;
 
         tail.AddComponent<OnTouchedByEnemy>();
         tail.tag = "Tail";
