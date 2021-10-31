478b53e9653056aab9c356e0fc42d13fbffe5f64
dlanaras <dimitrislanaras04@outlook.com>
2021-10-31 16:54:53 +0100

    improved snakes movement and fixed bugs

--- Scripts/EnemyRandomMoves.cs
+++ Scripts/EnemyRandomMoves.cs
@@ -12,22 +12,25 @@ public class EnemyRandomMoves : MonoBehaviour
 
     public float RandomGenerationDelayInSec = 0.5f;
 
+    private Rigidbody2D rigidbody2D;
+
     void Start()
     {
-        randDir = Random.Range(-5f, 5f);
+        randDir = Random.Range (-5f, 5f);
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         timer += Time.deltaTime;
         if(randDir >= 2.5f) {
-            transform.Translate(Vector2.down * Time.deltaTime * speed);
+            rigidbody2D.AddForce(Vector2.down * Time.deltaTime * speed, ForceMode2D.Impulse);
         } else if(randDir <= 2.5 && randDir >= 0) {
-            transform.Translate(Vector2.left * Time.deltaTime * speed);
+            rigidbody2D.AddForce(Vector2.left * Time.deltaTime * speed, ForceMode2D.Impulse);
         } else if(randDir >= -2.5 && randDir <= 0) {
-            transform.Translate(Vector2.up * Time.deltaTime * speed);
+            rigidbody2D.AddForce(Vector2.up * Time.deltaTime * speed, ForceMode2D.Impulse);
         } else if(randDir <= -2.5) {
-            transform.Translate(Vector2.right * Time.deltaTime * speed);
+            rigidbody2D.AddForce(Vector2.right * Time.deltaTime * speed, ForceMode2D.Impulse);
         }
         
         if(timer > RandomGenerationDelayInSec)
